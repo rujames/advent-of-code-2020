e899257ff88a6ab27cf7266a766467e27867a026
Ruairidh James <ruairidh.james@symphonicsoft.com>
2020-12-02 11:24:10 +0000

    Day 2 - Part 2!

--- src/advent_of_code_2020/day2.clj
+++ src/advent_of_code_2020/day2.clj
@@ -42,3 +42,35 @@ How many passwords are valid according to their policies?
 
 ;; (part-one)
 ;; => 572
+
+"
+--- Part Two ---
+While it appears you validated the passwords correctly, they don't seem to be what the Official Toboggan Corporate Authentication System is expecting.
+
+The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
+
+Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of \"index zero\"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+
+Given the same example list from above:
+
+1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+How many passwords are valid according to the new interpretation of the policies?
+"
+
+(defn verify-positions [[positions character password]]
+  (= 1
+     (->> positions
+          (map #(get password (dec %)))
+          (filter #(= character %))
+          (count))))
+
+(defn part-two []
+  (->> (str/split (slurp "src/advent_of_code_2020/day2.input") #"\r\n")
+       (map parse)
+       (filter verify-positions)
+       (count)))
+
+;; (part-two)
+;; => 419
