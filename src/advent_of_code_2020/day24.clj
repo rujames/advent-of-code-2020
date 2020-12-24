c2496749b8f1fdfb45011715f57a7723ac1efdde
Ru James <ruairidh.d.james@gmail.com>
2020-12-24 10:18:25 +0000

    Day 24 - Part 2!

--- src/advent_of_code_2020/day24.clj
+++ src/advent_of_code_2020/day24.clj
@@ -1,5 +1,6 @@
 (ns advent-of-code-2020.day24
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
 
 "
 --- Day 24: Lobby Layout ---
@@ -61,16 +62,72 @@ Go through the renovation crew's list and determine which tiles they need to fli
 
 (def input (map parse-instructions (str/split (slurp "src/advent_of_code_2020/day24.input") #"\r\n")))
 
+(defn offset [[x y] direction]
+  (condp = direction
+    :east [(inc x) y]
+    :west [(dec x) y]
+    :southeast [(+ x (mod y 2)) (dec y)]
+    :southwest [(- x (mod (inc y) 2)) (dec y)]
+    :northeast [(+ x (mod y 2)) (inc y)]
+    :northwest [(- x (mod (inc y) 2)) (inc y)]))
+
 (defn identified-tile [instructions]
-  (reduce (fn [[x y] instruction]
-            (condp = instruction
-              :east [(inc x) y]
-              :west [(dec x) y]
-              :southeast [(+ x (mod y 2)) (dec y)]
-              :southwest [(- x (mod (inc y) 2)) (dec y)]
-              :northeast [(+ x (mod y 2)) (inc y)]
-              :northwest [(- x (mod (inc y) 2)) (inc y)]))
-          [0 0] instructions))
+  (reduce offset [0 0] instructions))
 
 ;; (count (filter #(= 1 (mod % 2)) (vals (frequencies (map identified-tile input)))))
 ;; => 455
+
+"
+--- Part Two ---
+The tile floor in the lobby is meant to be a living art exhibit. Every day, the tiles are all flipped according to the following rules:
+
+Any black tile with zero or more than 2 black tiles immediately adjacent to it is flipped to white.
+Any white tile with exactly 2 black tiles immediately adjacent to it is flipped to black.
+Here, tiles immediately adjacent means the six tiles directly touching the tile in question.
+
+The rules are applied simultaneously to every tile; put another way, it is first determined which tiles need to be flipped, then they are all flipped at the same time.
+
+In the above example, the number of black tiles that are facing up after the given number of days has passed is as follows:
+
+Day 1: 15
+Day 2: 12
+Day 3: 25
+Day 4: 14
+Day 5: 23
+Day 6: 28
+Day 7: 41
+Day 8: 37
+Day 9: 49
+Day 10: 37
+
+Day 20: 132
+Day 30: 259
+Day 40: 406
+Day 50: 566
+Day 60: 788
+Day 70: 1106
+Day 80: 1373
+Day 90: 1844
+Day 100: 2208
+After executing this process a total of 100 times, there would be 2208 black tiles facing up.
+
+How many tiles will be black after 100 days?
+"
+
+(defn neighbours [tile]
+  (into #{} (map #(offset tile %) [:east :west :northeast :northwest :southeast :southwest])))
+
+(def black-tiles (into #{} (keep (fn [[tile n]] (if (= 1 (mod n 2)) tile)) (seq (frequencies (map identified-tile input))))))
+
+(defn step-tile [black-tiles tile]
+  (let [black-neighbours (count (set/intersection black-tiles (neighbours tile)))]
+    (if (black-tiles tile)
+      (if (#{1 2} black-neighbours) tile)
+      (if (#{2} black-neighbours) tile))))
+
+(defn step [black-tiles]
+  (let [to-check (set/union black-tiles (into #{} (mapcat neighbours black-tiles)))]
+    (into #{} (keep #(step-tile black-tiles %) to-check))))
+
+;; (count (nth (iterate step black-tiles) 100))
+;; => 3904
